2603230ce19102abd05146b3f45a2205e41728fa
Nikolai Balck <116642207+B4lck@users.noreply.github.com>
2025-02-27 12:52:44 +0100

    værsgo alle sammen

--- src/src/model/RecordAvailableState.java
+++ src/src/model/RecordAvailableState.java
@@ -1,4 +1,29 @@
 package model;
 
 public class RecordAvailableState implements RecordState {
+    private Record record;
+
+    public RecordAvailableState(Record record) {
+        this.record = record;
+    }
+
+    @Override
+    public void lend(String username) {
+        record.setState(new RecordLendedState(record, username));
+    }
+
+    @Override
+    public void reserve(String username) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void returnRecord() {
+
+    }
 }
--- src/src/model/RecordToRemoveState.java
+++ src/src/model/RecordToRemoveState.java
@@ -1,4 +1,29 @@
 package model;
 
 public class RecordToRemoveState implements RecordState {
+    private Record record;
+
+    public RecordToRemoveState(Record record) {
+        this.record = record;
+    }
+
+    @Override
+    public void lend(String username) {
+
+    }
+
+    @Override
+    public void reserve(String username) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void returnRecord() {
+
+    }
 }
