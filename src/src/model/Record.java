e0ff6d20da5c51fe26d35924bea8cc9b2c279098
Nikolai Balck <116642207+B4lck@users.noreply.github.com>
2025-02-27 11:35:04 +0100

    værsgo mazen

--- src/src/model/Record.java
+++ src/src/model/Record.java
@@ -1,4 +1,8 @@
 package model;
 
 public class Record {
+    private String title;
+    private String artist;
+    private int year;
+    private RecordState state;
 }
--- src/src/viewmodel/SimpleRecordViewModel.java
+++ src/src/viewmodel/SimpleRecordViewModel.java
@@ -1,4 +1,11 @@
 package viewmodel;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
 public class SimpleRecordViewModel {
+    private StringProperty titleProperty;
+    private StringProperty artistProperty;
+    private IntegerProperty yearProperty;
+    private StringProperty stateProperty;
 }
