2486a287e90c4466440b18bb3dae297199ef276c
Mazen <344698@via.dk>
2025-03-04 03:33:38 +0100

    Test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,5 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] args) {
         Application.launch(MyApplication.class);
-        System.out.println("Hello");
     }
     }
